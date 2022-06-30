f1e16b7742f9e610075f7dc0f083fc62a004d0b9
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2022-06-30 22:58:19 +0300

    correcting mistake

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-from sys import argv
-
 if __name__ == "__main__":
-    arg_num = len(argv) - 1
-    if arg_num == 0:
-        print("{}".format(arg_num))
-    else:
-        result = []
-        for i in range(1, arg_num + 1):
-            result.append(int(argv[i]))
-        print("{}".format(sum(result)))
+    import hidden_4
+    for s in dir(hidden_4):
+        if s[:2] != "__":
+            print("{:s}".format(s))
