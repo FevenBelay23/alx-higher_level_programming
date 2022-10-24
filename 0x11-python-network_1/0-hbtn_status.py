4006074de4ba99e79de867b5287a7c15747996d4
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2022-10-24 12:12:07 +0300

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""A script that
-fetches https://intranet.hbtn.io/status.
+"""
+This script fetchs  https://alx-intranet.hbtn.io/status
+The package used: urllib
 """
 
-
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        content = resp.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    from urllib import request
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as r:
+        print("""Body response:
+                     - type: {}
+                     - content: {}
+                     - utf8 content: {}""".format(type(r.read()),
+                                                  r.read(), r.msg))
