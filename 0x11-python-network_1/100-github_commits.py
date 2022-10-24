a0ce7ab833dc063f86ddd233d6627d1f76904ed6
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2022-10-24 12:08:53 +0300

    advanced

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-""" Github commits code challenge"""
+"""
+Get latest 10 commits from given owner and repo
+"""
 
-import requests
-from sys import argv
 
-if __name__ == '__main__':
-    url = "https://api.github.com/repos/{}/{}commits"\
-          .format(argv[2], argv[1])
-    r = requests.get(url)
-    commits = r.json()
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
 
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+    repo = argv[1]
+    owner = argv[2]
+    api_root = f"https://api.github.com/repos/{owner}/{owner}/commits"
+    res = get(api_root)
+    res = res.json()
+    for i in range(10):
+        sha = res[i].get("sha")
+        name = (res[i].get("commit").get("author").get("name"))
+        print("{}: {}".format(sha, name))
